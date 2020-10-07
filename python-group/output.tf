0c3964dcb5889af0952a2e49546fb82b97293512
e.proietti@goreply.io <e.proietti@goreply.io>
2020-10-07 23:46:36 +0200

    reordering

--- python-group/output.tf
+++
@@ -1,9 +0,0 @@
-output "targets" {
-  value = google_compute_region_instance_group_manager.pythonapp-group-manager[0].self_link
-}
-
-
-output "instance_group" {
-  value = google_compute_region_instance_group_manager.pythonapp-group-manager[0].instance_group
-  #value = google_compute_instance_group.unmanaged-python-pool[0].self_link
-}
