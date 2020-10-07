f9cb5dc4d310c1ea345d0ea69d3c40f9c923d3b3
e.proietti@goreply.io <e.proietti@goreply.io>
2020-10-08 00:04:40 +0200

    cleaning

--- terraform/python-group/output.tf
+++ terraform/python-group/output.tf
@@ -5,5 +5,4 @@ output "targets" {
 
 output "instance_group" {
   value = google_compute_region_instance_group_manager.pythonapp-group-manager[0].instance_group
-  #value = google_compute_instance_group.unmanaged-python-pool[0].self_link
 }
